c79bf5e54a2d558e2bc9f6c192fabf3c7d76f45f
kcsheng <kacysheng@mail.com>
2021-06-22 15:43:15 +1000

    Aysnc/await syntax explained

--- script.js
+++ script.js
@@ -9,18 +9,31 @@ function makeRequest(location) {
   })
 }
 
-function processRequest(response) {
+function processResponse(response) {
   return new Promise((resolve, reject) => {
     console.log("Processing response");
     resolve(`Desired information + ${response}`)
   })
 }
 
-makeRequest("Google").then(response => {
-  console.log("Request recieved!");
-  return processRequest(response);
-}).then(finalResult => {
-  console.log(finalResult);
-}).catch(err => {
-  console.log(err);
-})
+// makeRequest("Google").then(response => {
+//   console.log("Response recieved!");
+//   return processResponse(response);
+// }).then(finalResult => {
+//   console.log(finalResult);
+// }).catch(err => {
+//   console.log(err);
+// })
+
+async function getGoogleInfo() {
+  try {
+    const response = await makeRequest("Google");
+    console.log("Response received!");
+    const finalResult = await processResponse(response);
+    console.log(finalResult);
+  } catch(err) {
+    console.log(err);
+  };
+}
+
+getGoogleInfo();
